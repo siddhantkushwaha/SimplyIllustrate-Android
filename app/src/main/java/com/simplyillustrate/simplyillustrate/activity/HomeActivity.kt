1afcd582a09359a22b4c664c66fdda93b5de7120
Siddhant Kushwaha <k16.siddhant@gmail.com>
2019-02-07 12:02:08 +0530

    Sync call added for writeUser

--- app/src/main/java/com/simplyillustrate/simplyillustrate/activity/HomeActivity.kt
+++ app/src/main/java/com/simplyillustrate/simplyillustrate/activity/HomeActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.gson.JsonObject
 import com.simplyillustrate.simplyillustrate.R
 import com.simplyillustrate.simplyillustrate.api.RestAPI
 import com.simplyillustrate.simplyillustrate.pojo.User
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class HomeActivity : AppCompatActivity() {
 
@@ -23,28 +19,18 @@ class HomeActivity : AppCompatActivity() {
         val user = User()
         user.userId = FirebaseAuth.getInstance().uid
         user.email = FirebaseAuth.getInstance().currentUser!!.email
-        RestAPI.requestWriteUser(user, object : Callback<JsonObject> {
-
-            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-        })
-
-        user.name = "Siddhant Kushwaha"
-        user.phoneNumber = "+917351651000"
-        RestAPI.requestWriteUser(user, object : Callback<JsonObject> {
-
-            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-        })
+        RestAPI.requestWriteUser(user)
     }
+
+//    TODO - don't delete because this peice of code is often required
+//    object : Callback<JsonObject> {
+//
+//        override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+//
+//        }
+//
+//        override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+//
+//        }
+//    }
 }
--- app/src/main/java/com/simplyillustrate/simplyillustrate/api/RestAPI.java
+++ app/src/main/java/com/simplyillustrate/simplyillustrate/api/RestAPI.java
@@ -7,11 +7,14 @@ import com.google.gson.JsonObject;
 import com.simplyillustrate.simplyillustrate.pojo.User;
 import retrofit2.Call;
 import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Body;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+
+import java.io.IOException;
 
 public class RestAPI {
 
@@ -21,6 +24,7 @@ public class RestAPI {
     private static final Gson gson = new GsonBuilder().setLenient().create();
     private static final Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create(gson)).build();
 
+    //almost each api should have a synchronous and an asynchronous call
     private interface WriteUserApi {
 
         @Headers("Content-Type: application/json")
@@ -28,6 +32,15 @@ public class RestAPI {
         Call<JsonObject> writeUser(@Body User user);
     }
 
+    //synchronous call
+    public static Response<JsonObject> requestWriteUser(@NonNull User user) throws IOException {
+
+        WriteUserApi writeUserApi = retrofit.create(WriteUserApi.class);
+        Call<JsonObject> call = writeUserApi.writeUser(user);
+        return call.execute();
+    }
+
+    //asynchronous call
     public static void requestWriteUser(@NonNull User user, @NonNull Callback<JsonObject> callback) {
 
         WriteUserApi writeUserApi = retrofit.create(WriteUserApi.class);
