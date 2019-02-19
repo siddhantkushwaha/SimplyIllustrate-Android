1ae93eb4b7d37978ea87ffcf694e483dac25d2df
Siddhant Kushwaha <k16.siddhant@gmail.com>
2019-02-19 13:24:10 +0530

    Added fabric

--- app/src/main/java/com/simplyillustrate/simplyillustrate/SimplyIllustrate.java
+++ app/src/main/java/com/simplyillustrate/simplyillustrate/SimplyIllustrate.java
@@ -1,10 +1,13 @@
 package com.simplyillustrate.simplyillustrate;
 
 import android.app.Application;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class SimplyIllustrate extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
     }
 }
