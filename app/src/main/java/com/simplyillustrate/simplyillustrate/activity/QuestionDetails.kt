dbafdb8cb245695ec824f92f2953769a401c88ae
udayrajsawhney <udayraj.s16@iiits.in>
2019-05-03 01:16:03 +0530

    added onClick listener for detail activity along with basic UI for questions.

--- app/src/main/java/com/simplyillustrate/simplyillustrate/activity/QuestionDetails.kt
+++ app/src/main/java/com/simplyillustrate/simplyillustrate/activity/QuestionDetails.kt
@@ -3,11 +3,22 @@ package com.simplyillustrate.simplyillustrate.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.simplyillustrate.simplyillustrate.R
+import com.simplyillustrate.simplyillustrate.entity.Question
+import kotlinx.android.synthetic.main.activity_question_details.*
 
 class QuestionDetails : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question_details)
+
+        val bundle = intent.extras
+        if (bundle != null) {
+            val question = bundle.getParcelable<Question>("question")
+            question_title.text =  question.title
+            question_created_by.text = question.createdBy
+            question_description.text =  question.description
+            question_difficulty.text =  question.difficulty
+        }
     }
 }
